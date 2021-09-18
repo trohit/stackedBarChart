0f202a40efb7259c7caeabb1938c72ab6e373477
Rohit <trohit@users.noreply.github.com>
2021-09-18 11:20:37 +0530

    Update stackedBarChart.py

--- stackedBarChart.py
+++ stackedBarChart.py
@@ -9,20 +9,20 @@ from pprint import pprint
 #   Read cndl as Candle
 #   Read lbl  as Label
 #
-# In normal config 
+# In normal config
 # cndlsMasterLbl sits parallel to X Axis
 # valuesMasterLbl sits parallel to Y Axis
 #
 #               TITLE
 #      ^
-#      |  
+#      |
 #    Y |              +-+
 #      |              | |
 #      |        +-+   | |
 #      |  +-+   | |   | |
 #      |  | |   | |   | |
 #      +--+-+---+-+---+-+------->
-#       X Axis cndlsMasterLbl 
+#       X Axis cndlsMasterLbl
 #              Legend
 #       lyrColors -> lyrNames
 #
@@ -41,7 +41,7 @@ def stackedBarChart(Title, cndlsMasterLbl, valuesMasterLbl, cndlThickness,
     maxReading = maxValue + 1
     # gradual gradient markers to help read the values
     stepSize = (maxReading - minReading) /10
-    
+
     ind = np.arange(cndlCount)    # the x locations for the groups
     emptyArr = [0] * cndlCount
     sumArr = emptyArr
@@ -50,7 +50,7 @@ def stackedBarChart(Title, cndlsMasterLbl, valuesMasterLbl, cndlThickness,
     p = [] # list of bar properties
 
     plt.subplots_adjust(bottom=0.2) #make room for the legend
-    for i in xrange(len(arrVals)):
+    for i in range(len(arrVals)):
         selectedColor = lyrColors[i]
         print("Adding color " + selectedColor)
         tmpContainer = plt.bar(ind, arrVals[i], cndlThickness, color=selectedColor, bottom=graphBottom)
@@ -59,7 +59,7 @@ def stackedBarChart(Title, cndlsMasterLbl, valuesMasterLbl, cndlThickness,
         sumArr = [tmpArr[j] + sumArr[j] for j in range(cndlCount)]
         graphBottom = sumArr
         pprint(graphBottom)
-    
+
     plt.ylabel(valuesMasterLbl)
     plt.xlabel(cndlsMasterLbl)
     plt.title(Title)
@@ -82,19 +82,19 @@ def stackedBarChart(Title, cndlsMasterLbl, valuesMasterLbl, cndlThickness,
     # http://stackoverflow.com/questions/7745952/python-expand-list-to-function-arguments
     # http://stackoverflow.com/questions/12720450/unpacking-arguments-only-named-arguments-may-follow-expression
     plt.legend(
-         bbox_to_anchor=(0.5, -0.3), 
+         bbox_to_anchor=(0.5, -0.3),
          loc='lower center',
          ncol=7,labels=(lyrNames),*legArr)
-
     plt.grid()
-    plt.show()
+    #plt.show()
+    plt.savefig('books_read.png')
 
 if __name__ == "__main__":
     Title  = 'Visitors by block and day'
     cndlsMasterLbl = 'Date'
     valuesMasterLbl = 'No. of Visitors'
     cndlThickness = 0.30 # the cndlThickness of the bars: can also be len(x) sequence
-    #           Mo, Tu, We, Th, Fr, Sa, Su 
+    #           Mo, Tu, We, Th, Fr, Sa, Su
     blkCntAr = (10, 30, 30, 35, 27, 67, 21)
     blkCntBr = (20, 32, 34, 20, 25, 76, 21)
     blkCntEl = (30, 34, 34, 20, 25, 76, 21)
@@ -116,7 +116,7 @@ if __name__ == "__main__":
     cndlCount = len(blkCntAr) # count of elements in each category
     blocks=["Ar","Br","El","Gl","Hl","Jl","Jg","Zr","MB","MR","MT","Rs","Oh"]
     lyrNames = blocks
-    cndlLbls = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun') 
+    cndlLbls = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
     arrVals = blkCntAr,blkCntBr,blkCntEl,blkCntGl,blkCntHl,blkCntJg,blkCntJl,blkCntZr,blkCntMB,blkCntMR,blkCntMT,blkCntRs,blkCntOh
     lyrColors = ('r','g','b','c','m','y','k','Brown', 'Crimson','w','BlueViolet', 'DarkOrange','DeepPink')
     # days   -> cndls -> X
